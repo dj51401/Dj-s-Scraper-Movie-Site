fc5eac06b60c90ef487b2472086d5ce045c0c0e9
Dj51401's Desktop <dj51401@gmail.com>
2021-03-17 20:10:09 -0600

    Made alot faster!

--- automator.py
+++ automator.py
@@ -1,27 +1,41 @@
 import requests
 import pymongo
 import imdbscraper
+from multiprocessing import Process
 from bs4 import BeautifulSoup;
 
 client = pymongo.MongoClient("mongodb://localhost:27017")
 movieDatabase = client["MovieDatabase"]
 movieDB = movieDatabase["movies"]
+nextPage = ""
+url = "http://www.imdb.com/search/title/?title_type=feature&year=1950-01-01,2020-12-31&sort=num_votes,desc&start=1"
+work = []
 
-url = "http://www.imdb.com/search/title/?title_type=feature&year=1950-01-01,2020-12-31&sort=num_votes,desc&start=5051"
+#Sends links of individual movies to imdbscraper from a list of movies
+
 def sendLinks(url):
+    work.clear()
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
+    nextPageURL = soup.find("div", {"class" : "desc"}).findChild("a", {"class" : "next-page"}).get("href")
+    movieListElement = soup.findAll("div", {"class" : "lister-item"})
+    getWork(movieListElement)
+    procs = []
 
-    listerItem = soup.findAll("div", {"class" : "lister-item"})
+    for url in work:
+        proc = Process(target=imdbscraper.ScrapeIMDB, args=[url])
+        procs.append(proc)
+        proc.start()   
 
+    for proc in procs:
+        proc.join()
+
+    sendLinks("http://www.imdb.com" + nextPageURL)
+
+def getWork(listerItem):
     for i in range(len(listerItem)):
         listLink = listerItem[i].find("div", {"class" : "lister-item-image"}).findChild("a").get("href")
-        print("Scraping: http://www.imdb.com" + listLink)
-        imdbscraper.ScrapeIMDB("http://www.imdb.com" + listLink)
+        work.append("http://www.imdb.com" + listLink)
 
-    nextPage = soup.find("div", {"class" : "desc"}).findChild("a", {"class" : "next-page"})
-    print("Going to next page: http://www.imdb.com" + nextPage.get("href"))
-    sendLinks("http://www.imdb.com" + nextPage.get("href"))
-
-
-sendLinks(url)
+if(__name__ == "__main__"):
+    sendLinks(url)
--- imdbscraper.py
+++ imdbscraper.py
@@ -1,27 +1,39 @@
 import requests
 import pymongo
+from multiprocessing import Process
 from bs4 import BeautifulSoup
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 database = client["MovieDatabase"]
 movieDB = database["movies"]
 
-def ScrapeIMDB(url):
+def ProcessLinks(urlList):
+    if __name__ == "imdbscraper":
+        procs = []
+
+        for url in urlList:
+            proc = Process(target=ScrapeIMDB, args=(url,))
+            procs.append(proc)
+            proc.start()
+        
+        for proc in procs:
+            proc.join()
+
+def ScrapeIMDB(url):    
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
+    if(soup.find(id="title-overview-widget") != None):
 
-    titleWrapperElement = soup.find("div", {"class": "title_wrapper"})        
-    moviePoster = soup.find("div", {"class": "poster"}).findChild("a").findChild("img").get("src")
-    movieImdbRating = soup.find("div", {"class": "ratingValue"}).find("span").text.strip()
-    subtextElement = titleWrapperElement.findChild("div", {"class": "subtext"})
-    titleElement = titleWrapperElement.find('h1', {"class": ""})
-
-    if(titleElement != None):
+        titleWrapperElement = soup.find("div", {"class": "title_wrapper"})        
+        moviePoster = soup.find("div", {"class": "poster"}).findChild("a").findChild("img").get("src")
+        movieImdbRating = soup.find("div", {"class": "ratingValue"}).find("span").text.strip()
+        subtextElement = titleWrapperElement.findChild("div", {"class": "subtext"})
+        titleElement = titleWrapperElement.find('h1', {"class": ""})
         movieName = titleElement.find(text=True, recursive=False).strip()
         movieReleaseYear = titleElement.find(id="titleYear").findChild('a').text.strip()
-
         movieRating = subtextElement.find(text=True, recursive=False).strip()
         movieRuntime = subtextElement.find("time").text.strip()
+        movieSummary = soup.find("div", {"class": "summary_text"}).text.strip()
 
         movieReleaseDate = ''
         movieGenres = []
@@ -33,13 +45,12 @@ def ScrapeIMDB(url):
             else:
                 movieReleaseDate = genresList[element].text.strip()
 
-        movieSummary = soup.find("div", {"class": "summary_text"}).text.strip()
-    
-
+        print("scraped: " + movieName)
         movieDB.update_one(
-            {"movieName" : movieName},
+            {"_id" : movieName + movieReleaseYear},
             {
                 "$set" : {
+                    "_id" : movieName + movieReleaseYear,
                     "imdbLink": url,
                     "movieName": movieName,
                     "movieSummary": movieSummary,
@@ -56,5 +67,4 @@ def ScrapeIMDB(url):
                 }
             },
             upsert = True
-
         )
